266388ca3fb2129d3458983d186c79dc49d69292
miyo6032 <miyo6032@colorado.edu>
2019-10-06 17:12:27 -0600

    Insert Icosphere

--- ex13/ex13.c
+++ ex13/ex13.c
@@ -21,7 +21,7 @@
  *  +/-        Change field of view of perspective
  *  x          Toggle axes
  *  arrows     Change view angle
- *  PgDn/PgUp  Zoom in and out
+ *  1/2        Zoom in and out
  *  0          Reset view angle
  *  ESC        Exit
  */
@@ -50,6 +50,206 @@ int shininess =   0;  // Shininess (power of two)
 float shiny   =   1;  // Shininess (value)
 int zh        =  90;  // Light azimuth
 float ylight  =   0;  // Elevation of light
+
+void addVertices(float *vertices, int j, const float v1[3], const float v2[3], const float v3[3])
+{
+   vertices[j] = v1[0];
+   vertices[j + 1] = v1[1];
+   vertices[j + 2] = v1[2];
+   vertices[j + 3] = v2[0];
+   vertices[j + 4] = v2[1];
+   vertices[j + 5] = v2[2];   
+   vertices[j + 6] = v3[0];
+   vertices[j + 7] = v3[1];
+   vertices[j + 8] = v3[2];
+}
+
+void rescale(float radius, float v[3])
+{
+    float scale = radius / sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]);
+    v[0] *= scale;
+    v[1] *= scale;
+    v[2] *= scale;
+}
+
+// find middle point of 2 vertices
+// NOTE: new vertex must be resized, so the length is equal to the radius
+// from http://www.songho.ca/opengl/gl_sphere.html
+void computeHalfVertex(const float v1[3], const float v2[3], float newV[3])
+{
+    newV[0] = v1[0] + v2[0];    // x
+    newV[1] = v1[1] + v2[1];    // y
+    newV[2] = v1[2] + v2[2];    // z
+    rescale(1, newV);
+}
+
+void crossProduct(const float v1[3], const float v2[3], float newV[3])
+{
+   newV[0] = v1[1] * v2[2] - v1[2] * v2[1];
+   newV[1] = v1[2] * v2[0] - v1[0] * v2[2];
+   newV[2] = v1[0] * v2[1] - v1[1] * v2[0];
+}
+
+/*
+ * A function that returns interesting magnitudes to make the sphere into an animated blob
+ */
+float wave(const float v[3])
+{
+   return ((1.1 + sin(v[1]) * cos(v[2] * 2) * Sin(zh)) * 0.4);
+}
+
+/*
+ *  Draw icosphere
+ *     size
+ *     subivisions: number of times to divide the triangles from an icosahedron
+ *     animation: 1 if play a weird blobby animation
+ *
+ * CREDIT: Isophere code largely sourced from http://www.songho.ca/opengl/gl_sphere.html
+ */
+static void icosphere(float s, int subdivision, int animation)
+{
+   //  Vertex index list
+   int numIndices=60;
+   const unsigned char icoIndices[] =
+      {
+       2, 1, 0,    3, 2, 0,    4, 3, 0,    5, 4, 0,    1, 5, 0,
+      11, 6, 7,   11, 7, 8,   11, 8, 9,   11, 9,10,   11,10, 6,
+       1, 2, 6,    2, 3, 7,    3, 4, 8,    4, 5, 9,    5, 1,10,
+       2, 7, 6,    3, 8, 7,    4, 9, 8,    5,10, 9,    1, 6,10,
+      };
+
+   //  Vertex coordinates
+   float icoVertices[] =
+      {
+       0.000, 0.000, 1.000,
+       0.894, 0.000, 0.447,
+       0.276, 0.851, 0.447,
+      -0.724, 0.526, 0.447,
+      -0.724,-0.526, 0.447,
+       0.276,-0.851, 0.447,
+       0.724, 0.526,-0.447,
+      -0.276, 0.851,-0.447,
+      -0.894, 0.000,-0.447,
+      -0.276,-0.851,-0.447,
+       0.724,-0.526,-0.447,
+       0.000, 0.000,-1.000
+      };
+
+   /*
+    * We define temporary indices and vertices so that we can use them dynamically as the number of triangles increases
+    * with every subdivision. 
+    */
+   float *v1, *v2, *v3;
+   float * tempVertices;
+   int * tempIndices;
+   float newV1[3], newV2[3], newV3[3]; // new vertex positions
+   int * indices = malloc(numIndices * sizeof(int));
+   float * vertices = malloc(numIndices * 3 * sizeof(float));
+
+   // Orders the indices
+   for(int i = 0; i < numIndices; i++)
+   {
+      indices[i] = icoIndices[i];
+   }
+
+   // Order the vertices so that the they can be rendered as triangles in order
+   for(int i = 0; i < 12 * 3; i++)
+   {
+      vertices[i] = icoVertices[i];
+   }
+
+   // Subdivision algorithm
+   for(int i = 1; i <= subdivision; i++){
+
+      // Subdivides every triangle into 4 new triangles, so allocate 4 times the previous number of indices
+      int newIndices = numIndices * 4;
+      tempIndices = malloc(newIndices * sizeof(int));
+      tempVertices = malloc(newIndices * 3 * sizeof(float)); // Multiply by 3 because needs 3 coords to specify a single vertex
+
+      // Just order the indices in order
+      for (int j = 0; j < newIndices; j++)
+      {
+         tempIndices[j] = j;
+      }
+
+      for (int j = 0; j < numIndices; j+=3)
+      {
+         // get 3 vertices of an existing triangle
+         v1 = &vertices[indices[j] * 3];
+         v2 = &vertices[indices[j + 1] * 3];
+         v3 = &vertices[indices[j + 2] * 3];
+
+         // To the halfway points in between the three edges of the triangle and get the vertex at that point
+         computeHalfVertex(v1, v2, newV1);
+         computeHalfVertex(v2, v3, newV2);
+         computeHalfVertex(v1, v3, newV3);
+
+         // Adds three new vertices for each one of the four triangles created from subdiving the large triangle
+         // Obviously this is inefficient because we really only needs 6 vertices instead of 12, but whatever, it works
+         // with no major performance problems
+         int vertexStart = j * 3 * 4;
+         addVertices(tempVertices, vertexStart, v1, newV1, newV3);
+         addVertices(tempVertices, vertexStart + 9, newV1, v2, newV2);
+         addVertices(tempVertices, vertexStart + 18, newV1, newV2, newV3);
+         addVertices(tempVertices, vertexStart + 27, newV3, newV2, v3);
+      }
+
+      free(vertices);
+      free(indices);
+      vertices = tempVertices;
+      indices = tempIndices;
+      numIndices = newIndices;
+   }
+
+   // Basic rbg algorithm
+   float * rgb = malloc(numIndices * 3 * sizeof(float));
+   for (int i = 0; i < numIndices * 3; i++)
+   {
+      rgb[i] = 0.3;
+   }
+
+   // Animation done by rescaling the vertices based on the position of each vertex
+   if(animation){
+      for (int i = 0; i < numIndices * 3; i+=3)
+      {
+         rescale(wave(&vertices[i]), &vertices[i]);
+      }
+   }
+
+   glNormalPointer(GL_FLOAT, 0, vertices);
+   glEnableClientState(GL_NORMAL_ARRAY);
+   //  Define vertexes
+   glVertexPointer(3, GL_FLOAT, 0, vertices);
+   glEnableClientState(GL_VERTEX_ARRAY);
+   //  Define colors for each vertex
+   glColorPointer(3,GL_FLOAT,0,rgb);
+   glEnableClientState(GL_COLOR_ARRAY);
+   //  Draw icosahedron
+   glPushMatrix();
+   glScalef(s, s, s);
+   glDrawElements(GL_TRIANGLES, numIndices, GL_UNSIGNED_INT, indices);
+   glPopMatrix();
+   glDisableClientState(GL_NORMAL_ARRAY);
+   //  Disable vertex array
+   glDisableClientState(GL_VERTEX_ARRAY);
+   //  Disable color array
+   glDisableClientState(GL_COLOR_ARRAY);
+   free(vertices);
+   free(indices);
+   free(rgb);
+   glDisable(GL_LIGHTING);
+   float * normals = malloc(numIndices * 3 * sizeof(float)); // Multiply by 3 because needs 3 coords to specify a single vertex
+   for (int i = 0; i < numIndices * 3; i+=9)
+   {
+      // crossProduct(&vertices[i], &vertices[i + 3], &normals[i]);
+      // glBegin(GL_LINE_STRIP);
+      // glVertex3f(vertices[i], vertices[i + 1], vertices[i + 2]);
+      // glVertex3f(vertices[i + 3], vertices[i + 4], vertices[i + 5]);
+      // glVertex3f(vertices[i + 6], vertices[i + 7], vertices[i + 8]);
+      // glEnd();
+   }
+   free(normals);
+}
 
 /*
  *  Draw a cube
@@ -236,8 +436,9 @@ void display()
       glDisable(GL_LIGHTING);
 
    //  Draw scene
-   cube(+1,0,0 , 0.5,0.5,0.5 , 0);
-   ball(-1,0,0 , 0.5);
+   glPushMatrix();
+   icosphere(0.9, 0, 1);
+   glPopMatrix();
 
    //  Draw axes - no lighting from here on
    glDisable(GL_LIGHTING);
@@ -308,12 +509,6 @@ void special(int key,int x,int y)
    //  Down arrow key - decrease elevation by 5 degrees
    else if (key == GLUT_KEY_DOWN)
       ph -= 5;
-   //  PageUp key - increase dim
-   else if (key == GLUT_KEY_PAGE_DOWN)
-      dim += 0.1;
-   //  PageDown key - decrease dim
-   else if (key == GLUT_KEY_PAGE_UP && dim>1)
-      dim -= 0.1;
    //  Smooth color model
    else if (key == GLUT_KEY_F1)
       smooth = 1-smooth;
@@ -400,6 +595,12 @@ void key(unsigned char ch,int x,int y)
       shininess -= 1;
    else if (ch=='N' && shininess<7)
       shininess += 1;
+   //  PageUp key - increase dim
+   else if (ch == '1')
+      dim += 0.1;
+   //  PageDown key - decrease dim
+   else if (ch == '2')
+      dim -= 0.1;
    //  Translate shininess power to value (-1 => 0)
    shiny = shininess<0 ? 0 : pow(2.0,shininess);
    //  Reproject
