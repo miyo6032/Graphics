9711b51094941b13505c08f0a834ee5735b2009a
miyo6032 <miyo6032@colorado.edu>
2019-11-05 16:04:42 -0700

    Move shaders into separate files

---
+++ Project/shaders/sphere.vert
@@ -0,0 +1,14 @@
+uniform vec2 resolution;
+attribute vec4 center;
+attribute float radius;
+varying vec4 v_center;
+varying float v_radius;
+
+void main()
+{
+    v_radius = radius;
+    v_center = center * gl_ModelViewProjectionMatrix;
+    gl_PointSize = 2.0 + ceil(2.0*v_radius);
+    //gl_Position = vec4(2.0*center.xy/resolution-1.0, center.z, 1.0);
+    gl_Position = v_center;
+}
--- Project/visualizer.py
+++ Project/visualizer.py
@@ -10,10 +10,29 @@ import networkx as nx
 # CREDIT
 # https://www.labri.fr/perso/nrougier/python-opengl/#about-this-book
 # http://pyopengl.sourceforge.net/context/tutorials/shader_1.html
+# https://paroj.github.io/gltut/index.html
 
 class Renderer:
     def __init__(self, graph):
         self.graph = graph
+
+    # Loads the shader from files attempts to compile them, and returns the shader program
+    def read_shaders(self, vertex_file, fragment_file):
+        with open("./shaders/" + vertex_file, 'r') as file:
+            vertex_code = file.read()
+        with open("./shaders/" + fragment_file, 'r') as file:
+            fragment_code = file.read()
+
+        # Compile the shaders
+        try:
+            vertex_shader = shaders.compileShader(vertex_code, gl.GL_VERTEX_SHADER)
+            fragment_shader = shaders.compileShader(fragment_code, gl.GL_FRAGMENT_SHADER)
+            shader = shaders.compileProgram(vertex_shader,fragment_shader)
+            return shader
+        except(gl.GLError, RuntimeError) as err:
+            print("Shader Compilation Error:")
+            print(err)
+            exit()
 
     def render(self, resolution, tick):
         pass
@@ -24,54 +43,8 @@ class SpringNetworkRenderer(Renderer):
         super().__init__(graph)
         self.sphere_radius = 10
         self.sphere_data = np.array([[0, 0, 1]], 'f')
-        self.vertex_code = """
-            uniform vec2 resolution;
-            attribute vec4 center;
-            attribute float radius;
-            varying vec4 v_center;
-            varying float v_radius;
-            void main()
-            {
-                v_radius = radius;
-                v_center = center;
-                gl_PointSize = 2.0 + ceil(2.0*radius);
-                //gl_Position = gl_ModelViewProjectionMatrix * vec4(2.0*center.xy/resolution-1.0, center.z, 1.0);
-                gl_Position = center * gl_ModelViewProjectionMatrix;
-            }
-        """
 
-        self.fragment_code = """
-            varying vec4 v_center;
-            varying float v_radius;
-            void main()
-            {
-                vec4 global_ambient = vec4(0.3, 0.0, 0.0, 0.0);
-                vec2 p = (gl_FragCoord.xy - v_center.xy)/v_radius;
-                float z = 1.0 - length(p);
-                //if (z < 0.0) discard;
-
-                // Specify the actual depth the fragment is supposed to be at  
-                // (otherwise it is at v_center by default)
-                gl_FragDepth = 0.5*v_center.z + 0.5*(1.0 - z);
-
-                vec3 color = vec3(1.0, 0.0, 0.0);
-                vec3 normal = normalize(vec3(p.xy, z));
-                vec3 direction = normalize(vec3(1.0, 1.0, 1.0)); // Direction of the light
-                float diffuse = max(0.0, dot(direction, normal));
-                float specular = pow(diffuse, 24.0);
-                gl_FragColor = global_ambient + vec4(max(diffuse*color, specular*vec3(1.0)), 1.0);
-            }
-        """
-
-        # Compile the shaders
-        try:
-            vertex_shader = shaders.compileShader(self.vertex_code, gl.GL_VERTEX_SHADER)
-            fragment_shader = shaders.compileShader(self.fragment_code, gl.GL_FRAGMENT_SHADER)
-            self.shader = shaders.compileProgram(vertex_shader,fragment_shader)
-        except(gl.GLError, RuntimeError) as err:
-            print("Shader Compilation Error:")
-            print(err)
-            exit()
+        self.shader = self.read_shaders("sphere.vert", "sphere.frag")
 
         self.locations = {}
 
@@ -109,7 +82,7 @@ class SpringNetworkRenderer(Renderer):
                 gl.glUniform2f( self.locations["resolution"], *resolution )
                 gl.glEnableVertexAttribArray( self.locations["center"] )
                 gl.glEnableVertexAttribArray( self.locations["radius"] )
-                stride = len(scaled_data[0])*4 # 5 items per row, and each row is 4 bytes
+                stride = len(scaled_data[0])*4 # n items per row, and each row is 4 bytes
                 gl.glVertexAttribPointer(
                     self.locations["center"],
                     position_values, gl.GL_FLOAT,False, stride, sphere_vbo
@@ -125,80 +98,6 @@ class SpringNetworkRenderer(Renderer):
                 sphere_vbo.unbind()
                 gl.glDisableVertexAttribArray( self.locations["center"] )
                 gl.glDisableVertexAttribArray( self.locations["radius"] )
-        finally:
-            shaders.glUseProgram(0) # Go back to the legacy pipeline
-
-class FogRenderer(Renderer):
-    def __init__(self, graph):
-        super().__init__(graph)
-        vertex_code = """
-        uniform float end_fog;
-        uniform vec4 fog_color;
-        void main()
-        {
-            float fog; // amount of fog to apply
-            float fog_coord; // distance for fog calculation...
-            gl_Position = ftransform();
-            fog_coord = abs(gl_Position.z);
-            fog_coord = clamp( fog_coord, 0.0, end_fog);
-            fog = (end_fog - fog_coord)/end_fog;
-            fog = clamp( fog, 0.0, 1.0);
-            gl_FrontColor = mix(fog_color, gl_Color, fog);
-        }
-        """
-
-        fragment_code = """
-        void main()
-        {
-            gl_FragColor = gl_Color;
-        }
-        """
-
-        data = np.array( [
-            [  0, 1, 0,  0,1,0 ],
-            [ -1,-1, 0,  1,1,0 ],
-            [  1,-1, 0,  0,1,1 ],
-            [  2,-1, 0,  1,0,0 ],
-            [  4,-1, 0,  0,1,0 ],
-            [  4, 1, 0,  0,0,1 ],
-            [  2,-1, 0,  1,0,0 ],
-            [  4, 1, 0,  0,0,1 ],
-            [  2, 1, 0,  0,1,1 ],
-            ],'f')
-        # data = np.array([np.concatenate((v[:3] * 0.25, v[3:])) for v in data], 'f')
-        self.vbo = vbo.VBO(data) # Turn it into a format that opengl can use
-
-        try:
-            vertex_shader = shaders.compileShader(vertex_code, gl.GL_VERTEX_SHADER)
-            fragment_shader = shaders.compileShader(fragment_code, gl.GL_FRAGMENT_SHADER)
-            self.shader = shaders.compileProgram(vertex_shader,fragment_shader)
-        except(gl.GLError, RuntimeError) as err:
-            print("Shader Compilation Error:")
-            print(err)
-            exit()
-
-        self.locations = {
-            'end_fog': gl.glGetUniformLocation( self.shader, 'end_fog' ),
-            'fog_color': gl.glGetUniformLocation( self.shader, 'fog_color' ),
-        }
-
-    def render(self, resolution, tick):
-        shaders.glUseProgram(self.shader)
-        gl.glUniform1f(self.locations['end_fog'],10)
-        gl.glUniform4f(self.locations['fog_color'],0,0,0,1)
-        try:
-            self.vbo.bind()
-            try:
-                gl.glClear(gl.GL_COLOR_BUFFER_BIT)
-                gl.glEnableClientState(gl.GL_VERTEX_ARRAY);
-                gl.glEnableClientState(gl.GL_COLOR_ARRAY);
-                gl.glVertexPointer(3, gl.GL_FLOAT, 24, self.vbo)
-                gl.glColorPointer(3, gl.GL_FLOAT, 24, self.vbo + 12)
-                gl.glDrawArrays(gl.GL_TRIANGLES, 0, 9)
-            finally:
-                self.vbo.unbind()
-                gl.glDisableClientState(gl.GL_VERTEX_ARRAY);
-                gl.glDisableClientState(gl.GL_COLOR_ARRAY);
         finally:
             shaders.glUseProgram(0) # Go back to the legacy pipeline
 
@@ -275,7 +174,7 @@ class Context:
 
     def keyboard(self, key, x, y):
         if key == b'\x1b':
-            sys.exit( )
+            sys.exit()
 
     def idle(self):
         self.tick = glut.glutGet(glut.GLUT_ELAPSED_TIME);
@@ -283,5 +182,5 @@ class Context:
 graph = nx.fast_gnp_random_graph(10, 0.5)
 pos = nx.spring_layout(graph)
 context = Context(graph)
-context.renderers.append(FogRenderer(graph))
+context.renderers.append(SpringNetworkRenderer(graph))
 glut.glutMainLoop()
