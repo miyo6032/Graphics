890b7313cbcba7c98e34a4d35717ffcaba6c110d
miyo6032 <miyo6032@colorado.edu>
2019-11-12 10:08:00 -0700

    Quick clean up

--- Project/visualizer.py
+++ Project/visualizer.py
@@ -5,7 +5,6 @@ import OpenGL.GLU as glu
 from OpenGL.arrays import vbo
 from OpenGL.GL import shaders
 import glm
-from glm import *
 import numpy as np
 import networkx as nx
 
@@ -22,14 +21,14 @@ class Icosphere():
         Z = 0.850650808352039932
         N = 0.0
 
-        vertices = [vec3(-X,N,Z), vec3(X,N,Z), vec3(-X,N,-Z), vec3(X,N,-Z),
-                    vec3(N,Z,X), vec3(N,Z,-X), vec3(N,-Z,X), vec3(N,-Z,-X),
-                    vec3(Z,X,N), vec3(-Z,X, N), vec3(Z,-X,N), vec3(-Z,-X, N)]
+        vertices = [glm.vec3(-X,N,Z), glm.vec3(X,N,Z), glm.vec3(-X,N,-Z), glm.vec3(X,N,-Z),
+                    glm.vec3(N,Z,X), glm.vec3(N,Z,-X), glm.vec3(N,-Z,X), glm.vec3(N,-Z,-X),
+                    glm.vec3(Z,X,N), glm.vec3(-Z,X, N), glm.vec3(Z,-X,N), glm.vec3(-Z,-X, N)]
 
-        triangles = [  vec3(0,4,1),vec3(0,9,4),vec3(9,5,4),vec3(4,5,8),vec3(4,8,1),
-                    vec3(8,10,1),vec3(8,3,10),vec3(5,3,8),vec3(5,2,3),vec3(2,7,3),
-                    vec3(7,10,3),vec3(7,6,10),vec3(7,11,6),vec3(11,0,6),vec3(0,1,6),
-                    vec3(6,1,10),vec3(9,0,11),vec3(9,11,2),vec3(9,2,5),vec3(7,2,11)]
+        triangles = [  glm.vec3(0,4,1),glm.vec3(0,9,4),glm.vec3(9,5,4),glm.vec3(4,5,8),glm.vec3(4,8,1),
+                    glm.vec3(8,10,1),glm.vec3(8,3,10),glm.vec3(5,3,8),glm.vec3(5,2,3),glm.vec3(2,7,3),
+                    glm.vec3(7,10,3),glm.vec3(7,6,10),glm.vec3(7,11,6),glm.vec3(11,0,6),glm.vec3(0,1,6),
+                    glm.vec3(6,1,10),glm.vec3(9,0,11),glm.vec3(9,11,2),glm.vec3(9,2,5),glm.vec3(7,2,11)]
 
         return vertices, triangles
 
@@ -40,7 +39,7 @@ class Icosphere():
         if not (first_index, second_index) in lookup:
             v_0 = vertices[int(first_index)]
             v_1 = vertices[int(second_index)]
-            v_new = normalize(v_0 + v_1)
+            v_new = glm.normalize(v_0 + v_1)
             vertices.append(v_new)
             lookup[(first_index, second_index)] = len(vertices) - 1
 
@@ -56,10 +55,10 @@ class Icosphere():
             for i in range(3):
                 mid.append(self.get_vertex_for_edge(lookup, vertices, triangle[i], triangle[(i + 1) % 3]))
 
-            result.append(vec3(triangle[0], mid[0], mid[2]))
-            result.append(vec3(triangle[1], mid[1], mid[0]))
-            result.append(vec3(triangle[2], mid[2], mid[1]))
-            result.append(vec3(mid[0], mid[1], mid[2]))
+            result.append(glm.vec3(triangle[0], mid[0], mid[2]))
+            result.append(glm.vec3(triangle[1], mid[1], mid[0]))
+            result.append(glm.vec3(triangle[2], mid[2], mid[1]))
+            result.append(glm.vec3(mid[0], mid[1], mid[2]))
 
         return result
 
@@ -166,28 +165,21 @@ class RenderSphere(Renderer):
 class SpringNetworkRenderer(Renderer):
     def __init__(self, graph):
         self.graph = graph
-        self.sphere_radius = 10
         self.sphere_data = np.array([[0, 0, 1]], 'f')
 
-        self.shader = self.read_shaders("sphere.vert", "sphere.frag")
-
-        self.locations = {}
         self.convertNodes()
         self.sphere_renderer = RenderSphere(radius=0.1, subdivisions=2)
 
     def convertNodes(self):
-        array = []
-        for pos in nx.spring_layout(self.graph, dim=3).values():
-            array.append([*pos, self.sphere_radius])
-
-        self.sphere_data = np.array(array, 'f')
+        positions = [pos for pos in nx.spring_layout(self.graph, dim=3).values()]
+        self.sphere_data = np.array(positions, 'f')
 
     def render(self, tick, offset, camera_pos, light_pos, view_mat, proj_mat):
         degree = np.round((tick * 0.1)) % 360;
         light_pos = (self.approxCos(degree) * 2, self.approxSin(degree) * 2, 1)
 
         for pos in self.sphere_data:
-            self.sphere_renderer.render(tick, pos[:3], camera_pos, light_pos, view_mat, proj_mat)
+            self.sphere_renderer.render(tick, pos, camera_pos, light_pos, view_mat, proj_mat)
 
         self.sphere_renderer.render(tick, light_pos, camera_pos, light_pos, view_mat, proj_mat)
 
@@ -206,16 +198,14 @@ class Context:
 
     def setup_camera(self):
         self.camera_pos = glm.vec3(0, 0, -3)
-        self.camera_target = glm.vec3(0)
-        self.camera_dir = self.camera_pos - self.camera_target
-        self.camera_right = glm.normalize(glm.cross(glm.vec3(0, 1, 0), self.camera_dir)) # Cross up and camera dir to get the right vector
-        self.camera_up = glm.cross(self.camera_dir, self.camera_right) # Camera up direction
+        camera_target = glm.vec3(0)
+        camera_dir = self.camera_pos - camera_target
 
         Ex = -2*self.dim*self.approxSin(self.angle)*self.approxCos(self.elevation);
         Ey = +2*self.dim*self.approxSin(self.elevation);
         Ez = +2*self.dim*self.approxCos(self.angle)*self.approxCos(self.elevation);
 
-        self.view_mat = glm.lookAt(vec3(Ex, Ey, Ez), self.camera_target, vec3(0, self.approxCos(self.elevation), 0))
+        self.view_mat = glm.lookAt(glm.vec3(Ex, Ey, Ez), camera_target, glm.vec3(0, self.approxCos(self.elevation), 0))
 
     def approxCos(self, angle):
         return np.cos(3.1415926/180*angle)
@@ -256,7 +246,6 @@ class Context:
     def display(self):
         gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
         gl.glEnable(gl.GL_DEPTH_TEST)
-        gl.glLoadIdentity();
 
         for renderer in self.renderers:
             renderer.render(self.tick, (0, 0, 0), self.camera_pos, None, self.view_mat, self.proj_mat)
